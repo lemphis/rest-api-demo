a11b8f0626ed3244bf6f4fcc78fbcd3ea1bb49c3
jaeho-jang <jaehojang@nate.com>
2021-04-28 21:27:31 +0900

    Initial commit

---
+++ src/test/java/me/jh/restapidemo/RestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jh.restapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
