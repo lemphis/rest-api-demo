f0a7e0181509fc566baa584eda55a9096287fb39
jaeho-jang <jaehojang@nate.com>
2021-05-13 21:07:41 +0900

    Add create events test

--- src/main/java/me/jh/restapidemo/events/Event.java
+++ src/main/java/me/jh/restapidemo/events/Event.java
@@ -13,16 +13,16 @@ import java.time.LocalDateTime;
 public class Event {
 
     private Long id;
-    private String name;
-    private String description;
-    private LocalDateTime beginEnrollmentDateTime;
-    private LocalDateTime closeEnrollmentDateTime;
-    private LocalDateTime beginEventDateTime;
-    private LocalDateTime endEventDateTime;
-    private String location;
-    private int basePrice;
-    private int maxPrice;
-    private int limitOfEnrollment;
+    private String name; // 이름
+    private String description; // 설명
+    private LocalDateTime beginEnrollmentDateTime; // 이벤트 등록 시간
+    private LocalDateTime closeEnrollmentDateTime; // 이벤트 닫은 시간
+    private LocalDateTime beginEventDateTime; // 이벤트 시작 일시
+    private LocalDateTime endEventDateTime; // 이벤트 종료 일시
+    private String location; // 위치 (없으면 온라인 모임)
+    private int basePrice; // 참가비
+    private int maxPrice; // 참가비
+    private int limitOfEnrollment; // 최대 참가 인원
     private boolean offline;
     private boolean free;
     private EventStatus eventStatus;
