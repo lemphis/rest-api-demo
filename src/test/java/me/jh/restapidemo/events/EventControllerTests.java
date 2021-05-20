0249095d86a9a5f441146fa9606ca5d0b05cfbb7
jaeho-jang <jaehojang@nate.com>
2021-05-20 21:59:46 +0900

    Add description to test methods

--- src/test/java/me/jh/restapidemo/events/EventControllerTests.java
+++ src/test/java/me/jh/restapidemo/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.jh.restapidemo.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("event")
@@ -52,6 +54,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @DisplayName("입력받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .name("event")
@@ -78,6 +81,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @DisplayName("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -88,6 +92,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @DisplayName("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("event")
