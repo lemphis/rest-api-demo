4b32e7a4e76e0937a3ec12a14383dfd456e9fd3d
jaeho-jang <jaehojang@nate.com>
2021-05-27 21:08:30 +0900

    Add profile link

--- src/main/java/me/jh/restapidemo/events/EventController.java
+++ src/main/java/me/jh/restapidemo/events/EventController.java
@@ -1,6 +1,7 @@
 package me.jh.restapidemo.events;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -43,6 +44,7 @@ public class EventController {
         EventResource eventResource = new EventResource(event);
         eventResource.add(linkTo(EventController.class).withRel("query-events"));
         eventResource.add(selfLinkBuilder.withRel("update-event"));
+        eventResource.add(Link.of("/docs/index.html#resources-event-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
     }
 
