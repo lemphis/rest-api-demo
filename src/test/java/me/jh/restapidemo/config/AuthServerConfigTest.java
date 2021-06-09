1c53f7ad0da05900528d4d56d4d123a7b7c65701
jaeho-jang <jaehojang@nate.com>
2021-06-09 22:59:44 +0900

    Add authentication server config

---
+++ src/test/java/me/jh/restapidemo/config/AuthServerConfigTest.java
@@ -0,0 +1,48 @@
+package me.jh.restapidemo.config;
+
+import me.jh.restapidemo.accounts.Account;
+import me.jh.restapidemo.accounts.AccountRole;
+import me.jh.restapidemo.accounts.AccountService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @DisplayName("인증 토큰을 발급받는 테스트")
+    public void getAuthToken() throws Exception {
+        String username = "aaa@bbb";
+        String password = "jh";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountService.saveAccount(account);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                    .with(httpBasic(clientId, clientSecret))
+                    .param("username", username)
+                    .param("password", password)
+                    .param("grant_type", "password"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
+    }
+
+}
