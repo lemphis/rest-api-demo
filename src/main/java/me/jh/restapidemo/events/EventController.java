94dd3cbf84c01bcffd9dd0655fdef05c2e5120a4
jaeho-jang <jaehojang@nate.com>
2021-06-30 22:15:14 +0900

    Add link when have principal

--- src/main/java/me/jh/restapidemo/events/EventController.java
+++ src/main/java/me/jh/restapidemo/events/EventController.java
@@ -1,6 +1,8 @@
 package me.jh.restapidemo.events;
 
 import lombok.RequiredArgsConstructor;
+import me.jh.restapidemo.accounts.Account;
+import me.jh.restapidemo.accounts.CurrentUser;
 import me.jh.restapidemo.config.ErrorsResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,7 +39,7 @@ public class EventController {
     private final EventValidator eventValidator;
 
     @PostMapping
-    public ResponseEntity<?> createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+    public ResponseEntity<?> createEvent(@RequestBody @Valid EventDto eventDto, Errors errors, @CurrentUser Account currentUser) {
         if (errors.hasErrors()) {
             return badRequest(errors);
         }
@@ -59,15 +62,20 @@ public class EventController {
     }
 
     @GetMapping
-    public ResponseEntity<PagedModel<EntityModel<Event>>> queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+    public ResponseEntity<PagedModel<EntityModel<Event>>> queryEvents(Pageable pageable,
+                                                                      PagedResourcesAssembler<Event> assembler,
+                                                                      @CurrentUser Account currentUser) {
         Page<Event> page = this.eventRepository.findAll(pageable);
         PagedModel<EntityModel<Event>> pagedModels = assembler.toModel(page, EventResource::new);
         pagedModels.add(Link.of("/docs/index.html#resources-events-list").withRel("profile"));
+        if (currentUser != null) {
+            pagedModels.add(linkTo(EventController.class).withRel("create-event"));
+        }
         return ResponseEntity.ok(pagedModels);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EntityModel<Event>> getEvent(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<Event>> getEvent(@PathVariable Long id, @CurrentUser Account currentUser) {
         Optional<Event> optionalEvent = this.eventRepository.findById(id);
         if (optionalEvent.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -76,11 +84,15 @@ public class EventController {
         Event event = optionalEvent.get();
         EventResource eventResource = new EventResource(event);
         eventResource.add(Link.of("/docs/index.html#resources-events-get").withRel("profile"));
+        if (event.getManager().equals(currentUser)) {
+            eventResource.add(linkTo(EventController.class).slash(event.getId()).withRel("update-event"));
+        }
+
         return ResponseEntity.ok(eventResource);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateEvent(@PathVariable Long id, @RequestBody @Valid EventDto eventDto, Errors errors) {
+    public ResponseEntity<?> updateEvent(@PathVariable Long id, @RequestBody @Valid EventDto eventDto, Errors errors, @CurrentUser Account currentUser) {
         if (errors.hasErrors()) {
             return this.badRequest(errors);
         }
@@ -96,6 +108,9 @@ public class EventController {
         }
 
         Event existingEvent = optionalEvent.get();
+        if (!existingEvent.getManager().equals(currentUser)) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
         eventMapper.toEntity(eventDto, existingEvent);
 
         Event savedEvent = eventRepository.save(existingEvent);
