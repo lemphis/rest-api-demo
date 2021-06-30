f3cf94a64a5cf39e2bda8905618096f5ee36ba11
jaeho-jang <jaehojang@nate.com>
2021-06-30 22:12:25 +0900

    Modify loadUserByUsername method to return AccountAdapter

--- src/main/java/me/jh/restapidemo/accounts/AccountService.java
+++ src/main/java/me/jh/restapidemo/accounts/AccountService.java
@@ -3,7 +3,6 @@ package me.jh.restapidemo.accounts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -30,7 +29,7 @@ public class AccountService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = accountRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
-        return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+        return new AccountAdapter(account);
     }
 
     private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
